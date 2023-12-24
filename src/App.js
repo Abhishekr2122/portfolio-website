7165ffb42456d894617915799bda620bdeeb6e13
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-12-24 12:14:46 +0530

    Adding sass to the project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div className="App">Welcome to the portfolio website project</div>;
+  return (
+    <div className="App">
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Parallax</section>
+      <section>Contact</section>
+    </div>
+  );
 }
 
 export default App;
