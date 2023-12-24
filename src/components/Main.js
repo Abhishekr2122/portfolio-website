a443e6d33063e949bb3a1f4da35989a62976e46c
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-12-25 01:14:11 +0530

    Styling the text container

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,18 @@
 import "./main.scss";
 
 export default function Main() {
-  return <div className="main">The main container</div>;
+  return (
+    <div className="main">
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>Abhishek Shinde</h2>
+          <h1>Web developer</h1>
+          <div className="buttons">
+            <button>See the Latest works</button>
+            <button>Down load my resume</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
