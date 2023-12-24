3dad07c1a969a887c772c689bc469c649959e930
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-12-24 13:52:21 +0530

    Animating the components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import "./navbar.scss";
-
+import { motion } from "framer-motion";
 export default function Navbar() {
   return (
     <div className="navbar">
       {/* sidebar */}
       <div className="wrapper">
-        <span>Abhishek</span>
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Abhishek Shinde
+        </motion.span>
         <div className="socials">
           <a href="#">
             <img src="/dribbble.png" alt="processing" />
