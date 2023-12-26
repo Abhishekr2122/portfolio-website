9b2bfd79baeec52987a1ac13f4b873727699801d
Abhishekr2122 <abhishekrshinde21@gmail.com>
2023-12-26 23:01:57 +0530

    Changing the animation setting of sidebar

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -1,3 +1,10 @@
 export default function Parallax() {
-  return <div className="parallax">Parallax</div>;
+  return (
+    <div className="parallax">
+      <h1>Projects</h1>
+      <div className="mountains">mountains</div>
+      <div className="planets">planets</div>
+      <div className="stars">stars</div>
+    </div>
+  );
 }
