2df0e1db9da42a518e50a73e450ca1b3dfb983a1
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-01-03 21:47:46 +0530

    Creating projects section

--- src/components/Parallax.js
+++ src/components/Parallax.js
@@ -4,9 +4,9 @@ export default function Parallax() {
   return (
     <div className="parallax">
       <h1>Projects</h1>
-      <div className="mountains">mountains</div>
-      <div className="planets">planets</div>
-      <div className="stars">stars</div>
+      <div className="mountains"></div>
+      <div className="planets"></div>
+      <div className="stars"></div>
     </div>
   );
 }
