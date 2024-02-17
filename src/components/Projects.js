75b519283fe561ec922f2398d5d906753505320f
Abhishekr2122 <abhishekrshinde21@gmail.com>
2024-02-17 12:39:52 +0530

    Animating the Projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,15 +5,15 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "The Wild Oasis",
-    desc: "Its an hotel management app ",
-    image: "/worldwise.png",
-  },
-  {
-    id: 2,
     title: "The Fast Pizza App ",
     desc: "Its an pizza oedering app",
     image: "/projectscreenshot2.png",
+  },
+  {
+    id: 2,
+    title: "The Wild Oasis",
+    desc: "Its an hotel management app ",
+    image: "/worldwise.png",
   },
   {
     id: 3,
@@ -26,19 +26,22 @@ const items = [
 const Single = function ({ item }) {
   const ref = useRef();
 
-  const { scrollYProgress } = useScroll({ target: ref });
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"],
+  });
 
-  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+  const y = useTransform(scrollYProgress, [0, 1], [-120, 120]);
 
   return (
-    <section ref={ref}>
+    <section>
       <div className="container">
         <div className="wrapper">
-          <div className="imageContainer">
+          <div className="imageContainer" ref={ref}>
             <img src={item.image} alt="no data found" />
           </div>
 
-          <motion.div className="textContainer">
+          <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
             <button>Live project</button>
